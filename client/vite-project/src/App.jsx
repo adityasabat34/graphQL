b58f377ba7b83148dcca5bba146028701b268aeb
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-27 19:12:34 +0530

    fetch all user data from the server

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useQuery, useMutation, gql } from "@apollo/client";
+
+const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      name
+      age
+      isMarried
+    }
+  }
+`;
 
 const App = () => {
-  return <div>HEllo world</div>;
+  const { data, loading, error } = useQuery(GET_USERS);
+
+  if (loading) {
+    return <p>Data Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  return (
+    <>
+      <h1>Users</h1>
+
+      <div>
+        {data.getUsers.map((user) => (
+          <div key={user.id}>
+            <p>Name: {user.name}</p>
+            <p>age: {user.age}</p>
+            <p>isMarried: {user.isMarried ? "Yes" : "No"}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default App;
