493beb9f8e91126780aeb880bce03fb0a95ade32
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-02-27 13:03:54 +0530

    create typeDefs and mutation in graphQL

--- server/server.js
+++ server/server.js
@@ -1,9 +1,56 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const server = new ApolloServer({});
+const users = [
+  { id: 1, name: "Aditya", age: 34, isMarried: false },
+  { id: 2, name: "Shubh", age: 24, isMarried: true },
+  { id: 3, name: "Soham", age: 54, isMarried: true },
+  { id: 4, name: "Sahil", age: 32, isMarried: false },
+];
 
-const {} = await startStandaloneServer(server, {
+const typeDefs = `
+type Query {
+  getUsers: [User]
+  getUserById(id: ID!): User
+}
+
+type Mutation {
+  createUser(name: String!, age: Int!, isMarried: Boolean!): User
+}
+
+type User {
+  id: ID
+  name: String
+  age: Int
+  isMarried: Boolean
+}
+`;
+
+const resolvers = {
+  Query: {
+    getUsers: () => users,
+    getUserById: (parent, args) =>
+      users.find((user) => user.id === Number(args.id)),
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const { name, age, isMarried } = args;
+      const newUser = {
+        id: (users.length + 1).toString(),
+        name,
+        age,
+        isMarried,
+      };
+      users.push(newUser);
+      return newUser;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
-console.log(`Server Running at: ${url}`);
+
+console.log(`🚀 Server Running at: ${url}`);
